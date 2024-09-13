760aceadbf4a1ceea215bb06fda0f5fe68ede3b1
ziyad <mziyadbsk@gmail.com>
2024-09-13 14:58:03 +0300

    lesson6

---
+++ AND-OR.py
@@ -0,0 +1,10 @@
+A=int(input("write your first number "))
+B=int(input("write your second number "))
+if A>0 and B>0:
+    print("both is greater than 0")
+else:
+    print("both or one is less than 0")
+if A>0 or B>0:
+    print("one or both valare greater than 0")
+else:
+    print("both is less than 0")
